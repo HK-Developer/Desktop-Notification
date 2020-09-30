ccc77902bd5cb6133095b1cd7c9961a7ddd9d454
HK-Developer <72157311+HK-Developer@users.noreply.github.com>
2020-09-30 22:47:01 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import time
+from plyer import notification
+
+if __name__ == "__main__":
+    while True:
+        notification.notify(
+            title="Please Drink Water Now !",
+            message="The National Academies of Sciences, Engineering, and Medicine determined that an adequate daily fluid intake is: About 15.5 cups (3.7 liters) of fluids for men. About 11.5 cups (2.7 liters) of fluids a day for women.",
+            app_icon="D:\\1ST\Programming\Python projects\desktopNotification\Iconsmind-Outline-Glass-Water.ico",
+            timeout=10
+        )
+        # if you don't run it with window sheduler so you canrun it with pythonw
+        # and if you are running it on window shedular you can remove while loop and time.sleep ok
+        time.sleep(60*60)
